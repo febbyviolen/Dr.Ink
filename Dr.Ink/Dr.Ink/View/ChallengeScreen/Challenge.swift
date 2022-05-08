1efc2bd9e7172248eb49d1b3daddf8c704611975
inuinseoul <inwoo7233@naver.com>
2022-05-09 08:48:42 +0900

    feat: Challenge Screen 완성

---
+++ Dr.Ink/Dr.Ink/View/ChallengeScreen/Challenge.swift
@@ -0,0 +1,83 @@
+//
+//  Challenge.swift
+//  Dr.Ink
+//
+//  Created by Inwoo Park on 2022/05/09.
+//
+
+import SwiftUI
+
+struct Challenge: View {
+    @Binding var showModal: Bool
+    
+    var body: some View{
+        ZStack {
+            Background()
+            
+            VStack{
+                Text("챌린지")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("DarkDarkBlue"))
+                    .font(.title)
+                    .offset(y: -60)
+                    
+
+                Button(action: {
+                    showModal = true
+                }, label: {
+                    HStack(alignment: .center, spacing: 10) {
+                        Image("cok")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                        Text("무설탕으로 마시기")
+                            .font(.system(size: 22))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        Image("bub")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                    }
+                    .frame(width: 350, height: 160)
+                    .background(Color(red: 0.161, green: 0.349, blue: 0.51))
+                    .cornerRadius(30)
+                    .padding(.bottom, 40)
+                })
+               
+                
+                Button(action: {
+                    showModal = true
+                }, label: {
+                    HStack(alignment: .center, spacing: 5) {
+                        Image("cof")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                        Text("저카페인으로 마시기")
+                            .font(.system(size: 22))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        Image("tea")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                    }
+                    .frame(width: 350, height: 160)
+                    .background(Color(red: 0.435, green: 0.671, blue: 0.816))
+                    .cornerRadius(30)
+                    .padding(.bottom, 40)
+                })
+
+            }            
+        }
+    }
+}
+
+struct Challenge_Previews: PreviewProvider {
+    static var previews: some View {
+        Challenge(showModal: .constant(false))
+    }
+}
+
+
---
+++ Dr.Ink/Dr.Ink/View/ChallengeScreen/ChallengeModal.swift
@@ -0,0 +1,42 @@
+//
+//  ChallengeModal.swift
+//  Dr.Ink
+//
+//  Created by Inwoo Park on 2022/05/09.
+//
+
+import SwiftUI
+
+struct ChallengeModal: View {
+    
+    @Binding var isShowing: Bool
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            if isShowing {
+                Color.black
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        isShowing = false
+                    }
+                VStack {
+                    Text("Hello")
+                }
+                .frame(height: 400)
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .transition(.move(edge: .bottom))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeOut, value: isShowing)
+    }
+}
+
+struct ChallengeModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeModal(isShowing: .constant(true))
+    }
+}
