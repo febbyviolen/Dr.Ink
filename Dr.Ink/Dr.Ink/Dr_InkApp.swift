75d0100206df49be7ae1f76a13dd8c2049b277bd
inuinseoul <inwoo7233@naver.com>
2022-05-09 08:44:12 +0900

    add: 프로젝트 생성 및 gitignore 설정

---
+++ Dr.Ink/Dr.Ink/Dr_InkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dr_InkApp.swift
+//  Dr.Ink
+//
+//  Created by Inwoo Park on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct Dr_InkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
