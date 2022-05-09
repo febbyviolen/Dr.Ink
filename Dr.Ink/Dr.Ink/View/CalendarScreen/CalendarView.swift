0f2bead899024da04133121da774b5f5694e99bb
inuinseoul <inwoo7233@naver.com>
2022-05-09 19:27:19 +0900

    feat: CalendarView 임의설정

--- Dr.Ink/Dr.Ink/View/CalendarScreen/CalendarView.swift
+++ Dr.Ink/Dr.Ink/View/CalendarScreen/CalendarView.swift
@@ -12,15 +12,27 @@ struct CalendarView: View {
     var dailyWaterList: FetchedResults<DailyWater>
     
     var body: some View {
-        VStack {
-            Spacer()
-            List {
-                ForEach(dailyWaterList) { dailyWater in
-                    Text("\(dailyWater.date!)")
-                    Text("\(Int(dailyWater.intake)) / \(Int(dailyWater.goal))")
+        ZStack {
+            Background()
+            
+            VStack {
+                Text("기록")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("DarkDarkBlue"))
+                    .font(.title)
+                Divider()
+                ScrollView {
+                    LazyVStack {
+                        ForEach(dailyWaterList) { dailyWater in
+                            Text("\(dailyWater.date!)")
+                            Text("\(Int(dailyWater.intake)) / \(Int(dailyWater.goal))")
+                            Divider()
+                        }
+                    }                    
                 }
             }
-            Spacer()
+            .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: .infinity, alignment: .top)
+            
         }
     }
 }
