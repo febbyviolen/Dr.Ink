3d6f0917731d51f51b4833b7e930ed7693f28571
inuinseoul <inwoo7233@naver.com>
2022-05-17 12:58:22 +0900

    feat: 활동량 및 날씨변경 세팅 기능 추가

--- Dr.Ink/Dr.Ink/View/SettingScreen/ActivitySettingView.swift
+++ Dr.Ink/Dr.Ink/View/SettingScreen/ActivitySettingView.swift
@@ -9,9 +9,56 @@ import SwiftUI
 
 struct ActivitySettingView: View {
     @Binding var shouldShowModal: Bool
+    @EnvironmentObject var userSetting: UserSetting
     
     var body: some View {
-        Text("ActivitySettingView")
+        ZStack {
+            Color("LightLightBlue")
+                .opacity(0.3)
+            VStack {
+                Button(action: {
+                    userSetting.activity = .low
+                    shouldShowModal = false
+                }, label: {
+                    Text("낮음")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)
+                
+                Button(action: {
+                    userSetting.activity = .middle
+                    shouldShowModal = false
+                }, label: {
+                    Text("중간")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)
+                
+                Button(action: {
+                    userSetting.activity = .high
+                    shouldShowModal = false
+                }, label: {
+                    Text("높음")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)
+            }
+        }
     }
 }
 
--- Dr.Ink/Dr.Ink/View/SettingScreen/WeatherSettingView.swift
+++ Dr.Ink/Dr.Ink/View/SettingScreen/WeatherSettingView.swift
@@ -9,9 +9,70 @@ import SwiftUI
 
 struct WeatherSettingView: View {
     @Binding var shouldShowModal: Bool
+    @EnvironmentObject var userSetting: UserSetting
     
     var body: some View {
-        Text("WeatherSettingView")
+        ZStack {
+            Color("LightLightBlue")
+                .opacity(0.3)
+            VStack {
+                Button(action: {
+                    userSetting.weather = .hot
+                    shouldShowModal = false
+                }, label: {
+                    Text("더움")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)
+                
+                Button(action: {
+                    userSetting.weather = .warm
+                    shouldShowModal = false
+                }, label: {
+                    Text("따뜻함")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                
+                .cornerRadius(10)
+                Button(action: {
+                    userSetting.weather = .cool
+                    shouldShowModal = false
+                }, label: {
+                    Text("시원함")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)
+                
+                Button(action: {
+                    userSetting.weather = .cold
+                    shouldShowModal = false
+                }, label: {
+                    Text("추움")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                })
+                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+                .background(Color("Blue"))
+                .cornerRadius(10)                
+            }
+        }
     }
 }
 
